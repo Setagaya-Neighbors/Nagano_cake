1a3b82678540a64bfded7f7eed9b12d520d41461
EC2 Default User <ec2-user@ip-172-31-50-143.ec2.internal>
2021-10-20 08:56:28 +0000

    address update3

--- Nagano_cake/db/migrate/20211020071928_create_addresses.rb
+++ Nagano_cake/db/migrate/20211020071928_create_addresses.rb
@@ -2,12 +2,19 @@ class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
       
+      #FK#
+       t.reference :customer_id, foreign_key: true
+      #FK#
+      
+      #追記カラム＃
       t.integer :customer_id,  null: false
       t.string  :name,         null: false
       t.string  :postal_code,  null: false
       t.string  :address,      null: false
-
-      t.timestamps
+　　　#追記カラム＃
+　　　t.datetime :created_at
+      t.datetime :update_at
+      #ここまで
     end
   end
 end
