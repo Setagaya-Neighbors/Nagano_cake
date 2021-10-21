238239e559d80791ea7c4864fd87e5224eb88cfc
EC2 Default User <ec2-user@ip-172-31-50-143.ec2.internal>
2021-10-21 08:18:43 +0000

    devise修正

--- Nagano_cake/app/controllers/application_controller.rb
+++ Nagano_cake/app/controllers/application_controller.rb
@@ -5,12 +5,12 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
       
-     added_attrs = [ :last_name,:first_name,:last_name_ruby,:first_name_ruby,:postal_code,:address,:phone_number,:encrypted_password]
+     params = [ :last_name,:first_name,:last_name_ruby,:first_name_ruby,:postal_code,:address,:phone_number,:encrypted_password]
   
   
   
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_up, keys: params
+    devise_parameter_sanitizer.permit :account_update, keys: params
+    devise_parameter_sanitizer.permit :sign_in, keys: params
   end
 end
