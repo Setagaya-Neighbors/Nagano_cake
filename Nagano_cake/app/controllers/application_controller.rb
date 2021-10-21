9b22ea06b652c68bc9e9cdff97733f5a1d4e46d9
EC2 Default User <ec2-user@ip-172-31-50-143.ec2.internal>
2021-10-21 07:30:18 +0000

    devise3

--- Nagano_cake/app/controllers/application_controller.rb
+++ Nagano_cake/app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
      added_attrs = [ :last_name,:first_name,:last_name_ruby,:first_name_ruby,:postal_code,:address,:phone_number,:encrypted_password]
   
   
+  
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
