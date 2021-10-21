2afaa95250e5b2303a7ec809bf5fbed52bce86c1
EC2 Default User <ec2-user@ip-172-31-50-143.ec2.internal>
2021-10-21 04:56:57 +0000

    firstcommit devise

--- Nagano_cake/app/controllers/application_controller.rb
+++ Nagano_cake/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_ruby,:first_name_ruby,:postal_code,:address,:phone_number,:encrypted_password])
+  end
 end
