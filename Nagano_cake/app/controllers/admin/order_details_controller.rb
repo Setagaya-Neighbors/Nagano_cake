834c18da71196dbb527d6fb5c897519cd8f3de32
TellmeforDWC <88820261+TellmeforDWC@users.noreply.github.com>
2021-10-23 15:08:42 +0900

    OrderDeatailController、updateアクションの修正
    if分岐条件を追加

--- Nagano_cake/app/controllers/admin/order_details_controller.rb
+++ Nagano_cake/app/controllers/admin/order_details_controller.rb
@@ -1,10 +1,15 @@
 class Admin::OrderDetailsController < ApplicationController
-
+  
   def update
     @order_detail = Orderdetail.find(params[:id])
-    @order_detail.update(order_detail_params)
-    flash[:createdflag] = true
-    redirect_to admin_order_path(@order_detail.order_id)
+    if @order_detail.update(order_detail_params)
+      flash[:createdflag] = true
+      redirect_to admin_order_path(@order_detail.order_id)
+    else
+      @order = Order.find(pramas[:id])
+      render "order/show"
+      flash[:false] = "製作ステータスの更新に失敗しました。もう一度お試し下さい。"
+    end
   end
 
   private
