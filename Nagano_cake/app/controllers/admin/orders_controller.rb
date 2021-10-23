84cb515039cd58653485de56945d74671172bd8a
TellmeforDWC <88820261+TellmeforDWC@users.noreply.github.com>
2021-10-23 15:11:42 +0900

    OrderControllerの修正
    if分岐条件の追加

--- Nagano_cake/app/controllers/admin/orders_controller.rb
+++ Nagano_cake/app/controllers/admin/orders_controller.rb
@@ -7,9 +7,13 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update(order_params)
-    flash[:createdflag] = true
-    redirect_to admin_order_path(@order.id)
+    if @order.update(order_params)
+      flash[:createdflag] = true
+      redirect_to admin_order_path(@order.id)
+    else 
+      flash[:false] = "注文ステータスの更新に失敗しました。もう一度お試し下さい。"
+      render :show
+    end    
   end
 
   private
